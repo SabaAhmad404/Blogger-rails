74efdf9c6614ad0d0947e98ee7496dad62811eb9
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-03 13:17:28 +0500

    updated model method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
     belongs_to :user, class_name: 'User'
     belongs_to :post, class_name: 'Post'
+
+    def comments_counter
+        post.update(CommentsCounter: post.comments.count)
+    end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
     belongs_to :user ,class_name: 'User'
     belongs_to :post, class_name: 'Post'
+
+
+      def likes_counter
+        post.update(LikesCounter: post.likes.count)
+      end
+
 end

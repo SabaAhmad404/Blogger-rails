63ea6733946e15347f5f756af38bc24d6b35c027
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-04 14:19:54 +0500

    processing data models

---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post< ApplicationRecord
+    belongs_to :author , class_name: 'User'
+    has_many :comments
+    has_many :likes
+
+    def posts_counter
+        author.increment!(:posts_count)
+    end
+     def most_recent_comment
+        comments.limit(5).order(created_at: :desc)
+     end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+first_user = User.create(name: 'lucas', photo: 'https://avatars.githubusercontent.com/u/41428579?v=4', bio: 'first person ')
+second_user = User.create(name: 'Saba', photo: 'https://avatars.githubusercontent.com/u/107524451?v=4', bio: 'second person')
+third_user = User.create(name: 'Andi', photo: 'https://avatars.githubusercontent.com/u/116770908?v=4', bio: 'third person ')
+
+
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'Hello', text: 'This is my second post')
+third_post = Post.create(author: second_user, title: 'Hello', text: 'This is my first post')
+fourth_post = Post.create(author: second_user, title: 'Hello', text: 'This is my second post')
+fifth_post = Post.create(author: second_user, title: 'Hello', text: 'This is my third post')
+seventh_post = Post.create(author: second_user, title: 'Hello', text: 'This is my fourth post')

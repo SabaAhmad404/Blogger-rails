d7f9f98211b67d1897d538ef3a22fa98512a84d1
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-04 19:04:01 +0500

    updated models

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,11 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
+  after_save :update_likes_counter
+
+  private
+
   def update_likes_counter
-    post.increment!(:likes_count)
+    post.increment!(:likes_counter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def posts_counter
-    author.increment!(:posts_count)
-  end
 
   def most_recent_comment
     comments.limit(5).order(created_at: :desc)
+  end
+
+
+  def posts_counter
+    author.increment!(:posts_counter)
   end
 end
